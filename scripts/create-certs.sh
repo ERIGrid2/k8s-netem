efeaace8a08f4ab773562a3517be40ed7bda8298
Steffen Vogel <post@steffenvogel.de>
2021-07-12 13:20:34 +0200

    add scripts for generating webhook certificates and testing

---
+++ scripts/create-certs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 3650 -subj "/C=DE/ST=NRW/L=Aachen/CN=k8s-netem-webhook.riasc-system.svc"
+
+kubectl -n riasc-system delete secret k8s-netem-webhook-certs || true
+kubectl -n riasc-system create secret tls k8s-netem-webhook-certs --cert=cert.pem --key=key.pem 
+
+echo "caBundle value: "
+base64 -w0 < cert.pem
---
+++ scripts/test-mutate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cat > pod.json <<EOF
+{
+    "request": {
+        "uid": "eb6a1f44-e2fd-11eb-8d99-7c507989889d",
+        "object": {
+            "apiVersion": "v1",
+            "kind": "Pod",
+            "metadata": {
+                "name": "example",
+                "labels": {
+                "component": "test",
+                "traffic-profile": "delay-jitter-to-quad-one"
+                }
+            },
+            "spec": {
+                "containers": [
+                    {
+                        "name": "busybox",
+                        "image": "busybox:1.25",
+                        "command": [
+                        "ping",
+                        "1.1.1.1"
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+}
+EOF
+
+curl -X POST \
+    --data @pod.json \
+    -H "Content-Type: application/json" \
+    http://localhost:5000/mutate
