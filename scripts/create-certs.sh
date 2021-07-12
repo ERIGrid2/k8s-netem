65342ab8e7ffab3e6e70b8247eb6b188e1056e35
Steffen Vogel <post@steffenvogel.de>
2021-07-12 14:55:49 +0200

    use SANs for certificates

--- scripts/create-certs.sh
+++ scripts/create-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 3650 -subj "/C=DE/ST=NRW/L=Aachen/CN=k8s-netem-webhook.riasc-system.svc"
+openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 3650 -config req.conf -extensions 'v3_req'
 
 kubectl -n riasc-system delete secret k8s-netem-webhook-certs || true
 kubectl -n riasc-system create secret tls k8s-netem-webhook-certs --cert=cert.pem --key=key.pem 
