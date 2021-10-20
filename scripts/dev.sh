ee82870a01b811addf6480132f439280268ec152
Steffen Vogel <post@steffenvogel.de>
2021-10-21 00:59:04 +0200

    update dev helpers

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,10 @@
+function last-pod-logs() {
+    kubectl get pods --sort-by=.metadata.creationTimestamp --selector=$1 --no-headers -o custom-columns=":metadata.name"
+    POD=$(kubectl get pods --sort-by=.metadata.creationTimestamp --selector=$1 --no-headers -o custom-columns=":metadata.name" | tail -n1)
+
+    kubectl logs -f ${POD} $2
+}
+
 function webhook-update() {
     minikube image build . -t erigrid/netem
     kubectl rollout restart deployment k8s-netem-webhook
@@ -6,14 +13,20 @@ function webhook-update() {
     webhook-logs
 }
 
-function webhook-logs() {
-    kubectl get pods --sort-by=.metadata.creationTimestamp --selector=app=k8s-netem-webhook --no-headers -o custom-columns=":metadata.name"
-    POD=$(kubectl get pods --sort-by=.metadata.creationTimestamp --selector=app=k8s-netem-webhook --no-headers -o custom-columns=":metadata.name" | tail -n1)
 
-    kubectl logs -f ${POD}
-}
+function example-run() {
+    minikube image build . -t erigrid/netem
 
-function run-example() {
     kubectl rollout restart deployment example
     kubectl rollout status deployment example
+
+    example-logs
+}
+
+function example-logs() {
+    last-pod-logs component=example k8s-netem
+}
+
+function webhook-logs() {
+    last-pod-logs app=k8s-netem-webhook
 }
